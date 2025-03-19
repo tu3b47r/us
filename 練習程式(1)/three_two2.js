f5fb9583ff706f520ff8f3eac8d2a91ad9b01140
tu3b47r <y7bv2j5@gmail.com>
2025-03-20 01:15:05 +0800

    Delete 練習程式(1) directory

--- 練習程式(1)/three_two2.js
+++
@@ -1,9 +0,0 @@
-var bt1 = document.getElementById('bt1');
-bt1.addEventListener('click', function(){
-
-    document.open('text/html');
-
-    document.write('Javascript證照')
-
-    document.close();
-},false);
--- 練習程式(1)/three_two6.js
+++
@@ -1,4 +0,0 @@
-var s2 = document.getElementById('s2');
-s2.innerHTML = '網頁標題:' + document.title + '<br>'
-               + '網頁URL:' + document.URL + '<br>'
-               + '網頁最後修改日期:' + document.lastModified;
