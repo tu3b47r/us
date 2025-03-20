88273e49d4f106f53a7b3fb996d2fb0b004034de
tu3b47r <y7bv2j5@gmail.com>
2025-03-21 00:38:10 +0800

    Delete 練習程式(1) directory

--- 練習程式(1)/three_three4.js
+++
@@ -1,9 +0,0 @@
-var bt5 = document.getElementById('bt5');
-
-bt5.addEventListener('click',function(){
-    alert('草尼馬很可愛');
-},false);
-
-bt5.addEventListener('click',function(){
-    alert('媽媽');
-},false);
--- 練習程式(1)/three_two9.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded',function(){
-/*這是用JavaScript寫一個事件監聽器指定'DOMContentLoaded'
-，當指定事件觸發時，函示就執行之程式語法*/
-
-    var userNam1 = document.getElementById('userNam1');
-    userNam1.focus();
-    /*這是用JavaScript寫一個此變數的重點之程式語法*/
-
-},false);
